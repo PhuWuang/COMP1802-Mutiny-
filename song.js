57bb1ce64ad0f2e5cdf0a9f690274c41fae2197e
PhuWuang <phuquang19122005@gmail.com>
2023-11-21 22:27:06 +0700

    new hoca

--- song.js
+++ song.js
@@ -4,6 +4,15 @@ let songs=[
     },
     {
         path:'music/hakuna-matata-from-the-lion-king.mp3'
+    },
+    {
+        path:'music/nhac-phim-sieu-nhan-gao-vietsub-lyric-gaoranger-hoero-yukio-yamagata-live.mp3'
+    },
+    {
+        path:'music/night cafe vibes - a lofi hip hop mix.mp3'
+    },
+    {
+        path:'music/cute lofi mix songs to help you be happy - 寛げる [ J A P A N E S E L O F I F U T U R E B A S S].mp3'
     }
 
 
